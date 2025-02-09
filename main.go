b17f7250829d0f599447312d346f3b4b4c50c79b
Александр Лобов <aisanekai@mail.ru>
2025-02-09 15:02:43 +0400

    Debug information removed

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	_ "website/routers"
-	"website/utils"
 	_ "website/utils"
 
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
-	println(utils.Const.FLAG)
 	beego.Run("0.0.0.0:8000")
 }
